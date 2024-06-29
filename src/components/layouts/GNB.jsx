5a44d110d1444355aabafe4b8c0c4351daa5e4f2
minjuko <rhalswn0526@gmail.com>
2024-06-29 23:22:39 +0900

    feat: GNB 구현

---
+++ src/components/layouts/GNB.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import {
+  FaHome,
+  FaMapMarkedAlt,
+  FaChartBar,
+  FaUser,
+  FaCommentDots,
+} from "react-icons/fa";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  position: fixed;
+  bottom: 0;
+  z-index: 50;
+  width: 100%;
+  padding: 0 16px;
+  background-color: #f3f4f6;
+  border-top: 1px solid #e5e7eb;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  margin-right: 26px;
+`;
+
+const MenuLink = styled(Link)`
+  flex: 1;
+  text-align: center;
+  text-decoration: none;
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  color: ${({ isActive }) => (isActive ? "#4AAA87" : "#9ca3af")};
+`;
+
+const MenuText = styled.div`
+  font-size: 12px;
+  color: ${({ isActive }) => (isActive ? "#4AAA87" : "#6b7280")};
+`;
+
+const ChatIconWrapper = styled.div`
+  position: relative;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  background-color: ${({ isActive }) => (isActive ? "#4AAA87" : "#4AAA87")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -40px;
+  z-index: 1;
+`;
+
+const ChatIcon = styled(FaCommentDots)`
+  color: #fff;
+  font-size: 24px;
+`;
+
+export const GNB = () => {
+  const [currentPage, setCurrentPage] = useState("/");
+  const location = useLocation();
+
+  useEffect(() => {
+    setCurrentPage(location.pathname);
+  }, [location]);
+
+  return (
+    <Nav>
+      <Wrapper>
+        <MenuLink to="/" className={currentPage === "/" ? "active" : ""}>
+          <IconWrapper isActive={currentPage === "/"}>
+            <FaHome size={24} />
+          </IconWrapper>
+          <MenuText isActive={currentPage === "/"}>홈</MenuText>
+        </MenuLink>
+        <MenuLink to="/map" className={currentPage === "/map" ? "active" : ""}>
+          <IconWrapper isActive={currentPage === "/map"}>
+            <FaMapMarkedAlt size={24} />
+          </IconWrapper>
+          <MenuText isActive={currentPage === "/map"}>토양</MenuText>
+        </MenuLink>
+        <ChatIconWrapper isActive={currentPage === "/chat"}>
+          <ChatIcon />
+        </ChatIconWrapper>
+        <MenuLink
+          to="/analytics"
+          className={currentPage === "/analytics" ? "active" : ""}
+        >
+          <IconWrapper isActive={currentPage === "/analytics"}>
+            <FaChartBar size={24} />
+          </IconWrapper>
+          <MenuText isActive={currentPage === "/analytics"}>분석</MenuText>
+        </MenuLink>
+        <MenuLink
+          to="/profile"
+          className={currentPage === "/profile" ? "active" : ""}
+        >
+          <IconWrapper isActive={currentPage === "/profile"}>
+            <FaUser size={24} />
+          </IconWrapper>
+          <MenuText isActive={currentPage === "/profile"}>프로필</MenuText>
+        </MenuLink>
+      </Wrapper>
+    </Nav>
+  );
+};
---
+++ src/components/layouts/MainLayout.jsx
@@ -0,0 +1,35 @@
+import { Outlet } from "react-router-dom";
+import { GNB } from "./GNB";
+import styled from "styled-components";
+
+// 스타일링된 컴포넌트 정의
+const Main = styled.main`
+  margin-bottom: 0px;
+`;
+
+export const MainLayout = () => {
+  /* 로그인 후 토큰 가져오면 보여질 메인 레이아웃 (상단바, 하단바)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(getUserInfoThunk())
+        .then(unwrapResult)
+        .catch(() => {
+          localStorage.removeItem("token");
+          navigate("/login");
+        });
+    }
+  }, [dispatch, navigate]);
+  */
+
+  return (
+    <>
+      <Main>
+        <Outlet />
+      </Main>
+      <GNB />
+    </>
+  );
+};
