72f6a7a893ee7ab43dcf704b42a852c8c24cf3ef
leeseunghyen <2shyen6@naver.com>
2024-07-04 16:59:23 +0900

    first commit

--- aivle_big/urls.py
+++ aivle_big/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', index),
     path("admin/", admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('soil/', include('soil.urls')),
 ]
---
+++ soil/views.py
@@ -0,0 +1,6 @@
+# soil/views.py
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'soil.html')
